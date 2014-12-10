a35da22269a376db42725440ca5ee0fbd47f4de4
Matt Levine <mtlevine0@gmail.com>
2014-12-09 23:35:38 -0500

    Updated README.md and created liscense.txt.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright (c) 2014
+//
+// Matt Levine
+// Created: 12/9/14
+//
 
 // Create selection context
 var title = "Search Urban Dictionary for 'selection'";
